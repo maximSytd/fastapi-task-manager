7cb7cab02a455b9b60ca7e19c7966a6cedb1886e
Maxim Sytdikov <maximovichpro@gmail.com>
2025-08-22 22:54:08 +0500

    Add Docstrings

--- app/db/schemas/auth.py
+++ app/db/schemas/auth.py
@@ -2,9 +2,12 @@ import pydantic
 
 
 class Token(pydantic.BaseModel):
+    """OAuth Token schema."""
     access_token: str
     token_type: str
 
 class UserLogin(pydantic.BaseModel):
+    """User login schema."""
+
     username: str
     password: str
